5012113b9d25a012d2b8948a4046541e7d6d57c3
Neil Farrington <me@neilfarrington.com>
2019-04-29 00:37:53 +0100

    Add sorting functionality

--- src/main/java/xyz/nowiknowmy/hogwarts/helpers/Str.java
+++ src/main/java/xyz/nowiknowmy/hogwarts/helpers/Str.java
@@ -1,5 +1,8 @@
 package xyz.nowiknowmy.hogwarts.helpers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Str {
@@ -13,7 +16,23 @@ public class Str {
         return pattern.matcher(string).find();
     }
 
+    public List<String> groups(String string) {
+        Matcher m = pattern.matcher(string);
+        List<String> groups = new ArrayList<>();
+        if (m.find()) {
+            for (int i = 0; i <= m.groupCount(); i++) {
+                groups.add(m.group(i));
+            }
+        }
+
+        return groups;
+    }
+
     public static Str regex(String regex) {
         return new Str(Pattern.compile(regex));
+    }
+
+    public static Str regex(String regex, int flags) {
+        return new Str(Pattern.compile(regex, flags));
     }
 }
--- src/main/java/xyz/nowiknowmy/hogwarts/services/MessageService.java
+++ src/main/java/xyz/nowiknowmy/hogwarts/services/MessageService.java
@@ -2,7 +2,11 @@ package xyz.nowiknowmy.hogwarts.services;
 
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.MessageChannel;
+import discord4j.core.object.entity.Role;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import xyz.nowiknowmy.hogwarts.BotEventListener;
 import xyz.nowiknowmy.hogwarts.domain.Guild;
 import xyz.nowiknowmy.hogwarts.domain.Points;
 import xyz.nowiknowmy.hogwarts.helpers.Str;
@@ -12,12 +16,18 @@ import xyz.nowiknowmy.hogwarts.repositories.PointsRepository;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service
 public class MessageService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageService.class);
+
     private final GuildRepository guildRepository;
     private final PointsRepository pointsRepository;
 
@@ -35,7 +45,27 @@ public class MessageService {
         } else if (Str.regex("^!points ?.*$").matches(content)) {
             return sendPointsSummary(message.getChannel().block(), guild);
         } else if (Str.regex("^!(gryffindor|hufflepuff|ravenclaw|slytherin) ?.*$").matches(content)) {
-            throw new UnsupportedOperationException("Not implemented");
+            List<String> regexMatches = Str.regex("^!(gryffindor|hufflepuff|ravenclaw|slytherin) ?.*$").groups(content);
+            List<Role> existingHouse = message.getAuthorAsMember().block().getRoles().collectList().block()
+                    .stream().filter(role -> Str.regex("^(Gryffindor|Hufflepuff|Ravenclaw|Slytherin)$", Pattern.CASE_INSENSITIVE).matches(role.getName()))
+                    .collect(Collectors.toList());
+
+            if (existingHouse.size() > 0) {
+                sendError(message.getChannel().block(), "Sorry, you already have a house!");
+                return null;
+            }
+
+            Optional<Role> role = message.getGuild().block().getRoles().collectList().block()
+                    .stream().filter(role1 -> role1.getName().compareToIgnoreCase(regexMatches.get(1)) == 0)
+                    .findFirst();
+
+            if (role.isEmpty()) {
+                sendError(message.getChannel().block(), "Sorry, we could not find that role!");
+                return null;
+            }
+
+            message.getAuthorAsMember().block().addRole(role.get().getId()).block();
+            message.getChannel().block().createMessage(String.format("You are now a member of %s!", regexMatches.get(1))).block();
         } else if (Str.regex("^!([ghrs]) (add|sub|subtract|set) (\\d+)$").matches(content)) {
             throw new UnsupportedOperationException("Not implemented");
         } else if (Str.regex("^!inactive (\\d+[d|m|y])$").matches(content)) {
@@ -47,6 +77,10 @@ public class MessageService {
         }
 
         return null;
+    }
+
+    private void sendError(MessageChannel channel, String errorMessage) {
+        channel.createMessage(errorMessage).block();
     }
 
     private Void sendServerTime(MessageChannel channel) {
